bbda1369ee5dbe6db52085fc5ef0eb8dfe4382e7
Lisa Lee <snickers495@gmail.com>
2017-12-03 00:09:26 +0000

    automatically backed up by learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -12,7 +12,7 @@ class AuthorsController < ApplicationController
     if @author.valid?
       @author.save
       redirect_to author_path(@author)
-    else 
+    else
       render :new
     end 
   end
