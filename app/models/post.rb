9dba7e1683b78498c09b9090bbbbd52a88a93d1c
Lisa Lee <snickers495@gmail.com>
2017-12-03 00:13:59 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :category, inclusion: { is: %w(Fiction Non-Fiction)}, presence: true
-  validates :content, length: { maximum: 100}
+  validates :content, length: { maximum: 100}, presence: true
 end
