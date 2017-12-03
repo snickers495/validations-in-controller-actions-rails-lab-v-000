d5385b12bc9f8ae4928f10fdc19a97e1802a9ae6
Lisa Lee <snickers495@gmail.com>
2017-12-03 00:18:15 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,4 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
-  validates :category, inclusion: { is: %w(Fiction Non-Fiction)}, message: "This is need to be either Fiction or Non Fiction."
-  validates :content, length: { maximum: 100}, message: "This is need to be at least 100 characters."
-end
+  validates :category, inclusion: { is: %w(Fiction Non-Fiction)}
+  validates :content, length: { maximum: 100}
